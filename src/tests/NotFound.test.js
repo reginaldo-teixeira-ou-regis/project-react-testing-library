25667d54fd9e6863580895999a8829c02b0006e7
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2022-11-22 22:49:02 -0300

    lint

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { act } from 'react-dom/test-utils';
 import renderWithRouter from './renderWithRouter';
 import NotFound from '../pages/NotFound';
 
